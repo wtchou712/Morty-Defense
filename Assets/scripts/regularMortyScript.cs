2456c106aba8c8b8afcbeb1ae856c1ba0e3d676e
William Chou <wtchou712@gmail.com>
2016-02-07 16:01:19 -0600

    prefab Morty

--- Assets/scripts/regularMortyScript.cs
+++ Assets/scripts/regularMortyScript.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class regularMortyScript : MonoBehaviour {
+
 
 	// Use this for initialization
 	void Start () {
-	
+//		anim = gameObject.GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		this.transform.Translate (new Vector3 (5f * Time.deltaTime, 0f, 0f));
 	}
 }
