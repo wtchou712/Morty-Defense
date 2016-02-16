3d118d061e416142bb941ae7d387145313517ed6
Edward Hu <edwardhu2016@u.northwestern.edu>
2016-02-15 18:15:03 -0600

    healthbar UI added

---
+++ Assets/scripts/AllyTowerScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AllyTowerScript : MonoBehaviour {
+
+	public float maxHealth = 100f;
+	public float currentHealth = 0f;
+	public GameObject healthBar;
+
+
+	// Use this for initialization
+	void Start () {
+		currentHealth = maxHealth;
+		InvokeRepeating ("decreaseHealth", 1f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void decreaseHealth(){
+		currentHealth -= 2f;
+		float calculatedHealth = currentHealth / maxHealth;
+		setHealthBar (calculatedHealth);
+	}
+
+	public void setHealthBar(float myHealth){
+		
+		healthBar.transform.localScale = new Vector3(myHealth, healthBar.transform.localScale.y, healthBar.transform.localScale.z);
+	}
+}
